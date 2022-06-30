b3cb7ce6188aa04802e1a1d8ca4cfde826f58bc2
George Marques <georgemarquesfs@hotmail.com>
2022-06-30 11:07:32 -0300

    final project 1.0

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,22 @@
 import { Request, Response } from 'express';
+import { Pet } from '../models/pet'
+import { createMenuObject} from '../helpers/createMenuObject'
+
 
 export const search = (req: Request, res: Response) => {
+    let query: string = req.query.q as string;
 
+    if(!query){
+        res.redirect('/')
+        return
+    }
+
+    let list = Pet.getFromName(query) // para pega o que a pessoa digitou na consulta e selecionar os pets
+
+    res.render('pages/page', {
+        menu: createMenuObject(''),
+        list,
+        query
+
+    })
 }
